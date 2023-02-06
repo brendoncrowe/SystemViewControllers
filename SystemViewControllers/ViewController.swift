dbd886310923a73f4826bdd00502260948d3491b
Brendon Crowe <brendon.m.crowe@gmail.com>
2023-02-06 09:15:02 -0500

    Initial Commit

---
+++ SystemViewControllers/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Brendon Crowe on 2/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+ 
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func cameraButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+    }
+    
+}
+
